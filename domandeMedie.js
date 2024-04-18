4a32477d0d6092cb05710588bcca71e7bf1f1414
Nicola Schirripa <164496803+nicherri@users.noreply.github.com>
2024-04-18 15:16:07 +0200

    .

---
+++ domandeMedie.js
@@ -0,0 +1,190 @@
+const questionsMiddle = [
+    {
+        category: "Web Development",
+        type: "multiple",
+        difficulty: "middle",
+        question: "What is the purpose of HTML in web development?",
+        correct_answer: "Structure the content of a website",
+        incorrect_answers: [
+          "Add interactivity and dynamic elements to a website",
+          "Define the style and layout of a website",
+          "Make a website interactive",
+        ],
+      },
+      {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "middle",
+        question: "Which of the following is NOT a valid data type in JavaScript?",
+        correct_answer: "Array",
+        incorrect_answers: [
+          "Boolean",
+          "Number",
+          "String",
+        ],
+      },
+      {
+        category: "Web Development",
+        type: "multiple",
+        difficulty: "middle",
+        question: "What does the 'function' keyword signify in JavaScript?",
+        correct_answer: "Declaring a reusable block of code",
+        incorrect_answers: [
+          "A built-in method",
+          "A loop statement",
+          "A data type",
+        ],
+      },
+      {
+        category: "Web Development",
+        type: "multiple",
+        difficulty: "middle",
+        question: "In HTML, what tag is used to create a hyperlink?",
+        correct_answer: "<a> ",
+        incorrect_answers: [
+          "<src>",
+          "<link>",
+          "<href>",
+        ],
+      },
+      {
+        category: "Web Development",
+        type: "multiple",
+        difficulty: "middle",
+        question: "Which CSS property is used to create space between an element's border and its content?",
+        correct_answer: "Padding",
+        incorrect_answers: [
+          "Margin",
+          "Border-spacing",
+          "Space"
+        ],
+      },
+      {
+        category: "Web Development",
+        type: "multiple",
+        difficulty: "middle",
+        question: "What symbol is used for single-line comments in JavaScript?",
+        correct_answer: "// ",
+        incorrect_answers: [
+          "--!>",
+          "/*",
+          "<!--",
+        ],
+      },
+      {
+        category: "Web Development",
+        type: "multiple",
+        difficulty: "middle",
+        question: "What is the purpose of the <script> tag in HTML?",
+        correct_answer: "Write JavaScript code within an HTML document",
+        incorrect_answers: [
+          "Load external images",
+          "Style the content of a webpage",
+          "Link to an external CSS file",
+        ],
+      },
+      {
+        category: "Web Development",
+        type: "multiple",
+        difficulty: "middle",
+        question: "What JavaScript function is used to print content to the console for debugging?",
+        correct_answer: "A) console.log() ",
+        incorrect_answers: [
+          "print()",
+          "log()",
+          "write()",
+        ],
+      },
+      {
+        category: "Web Development",
+        type: "multiple",
+        difficulty: "middle",
+        question: "Which HTML tag is used to define the structure of a table?",
+        correct_answer: "<table>",
+        incorrect_answers: [
+          "<tr>",
+          "<row>",
+          "<tab>",
+        ],
+      },
+      {
+        category: "Web Development",
+        type: "boolean",
+        difficulty: "middle",
+        question: "HTML is responsible for the structure of a webpage, while CSS is responsible for the presentation of the content on the webpage.",
+        correct_answer: "True ",
+        incorrect_answers: ["False"]
+      },
+      {
+        category: "Web Development",
+        type: "boolean",
+        difficulty: "middle",
+        question: "JavaScript is a server-side scripting language that helps with the functionality of a website.",
+        correct_answer: "False",
+        incorrect_answers: ["True "]
+      },
+      {
+        category: "Web Development",
+        type: "boolean",
+        difficulty: "middle",
+        question: "In HTML, the '<head>' tag is where you would typically find the content that is directly displayed on the webpage.",
+        correct_answer: "False",
+        incorrect_answers: ["True "]
+      },
+      {
+        category: "Web Development",
+        type: "boolean",
+        difficulty: "middle",
+        question: "CSS can be directly embedded within an HTML file using the '<css>' tag.",
+        correct_answer: "False",
+        incorrect_answers: ["True "]
+      },
+      {
+        category: "Web Development",
+        type: "boolean",
+        difficulty: "middle",
+        question: "Inline CSS is when you apply CSS styling directly within the HTML element using the 'style' attribute.",
+        correct_answer: "True ",
+        incorrect_answers: ["False"]
+      },
+      {
+        category: "Web Development",
+        type: "boolean",
+        difficulty: "middle",
+        question: "HTML is not a programming language but a markup language used to structure content on the web.",
+        correct_answer: "True ",
+        incorrect_answers: ["False"]
+      },
+      {
+        category: "Web Development",
+        type: "boolean",
+        difficulty: "middle",
+        question: "JavaScript is mainly used for client-side scripting to make web pages interactive.",
+        correct_answer: "True",
+        incorrect_answers: ["False"]
+      },
+      {
+        category: "Web Development",
+        type: "boolean",
+        difficulty: "middle",
+        question: "CSS stands for Central Style Sheets, which is a way to globally style web pages across different websites.",
+        correct_answer: "False",
+        incorrect_answers: ["True "]
+      },
+      {
+        category: "Web Development",
+        type: "boolean",
+        difficulty: "middle",
+        question: "JavaScript is mandatory for creating interactive elements on a webpage, such as buttons that trigger specific actions.",
+        correct_answer: "True ",
+        incorrect_answers: ["False"]
+      },
+      {
+        category: "Web Development",
+        type: "boolean",
+        difficulty: "middle",
+        question: "The use of external CSS files allows for consistent styling across multiple HTML pages.",
+        correct_answer: "True ",
+        incorrect_answers: ["False"]
+      }
+    ];
