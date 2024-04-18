9f4852de328aa7d10a8be34e0c6866919c269f92
Nicola Schirripa <164496803+nicherri@users.noreply.github.com>
2024-04-18 16:07:06 +0200

    dimandefacile

---
+++ doamndeFacili.js
@@ -0,0 +1,110 @@
+const questionsEasy = [ 
+    {
+    category: "Computer Science",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What is a computer?",
+    correct_answer: "D) An electronic device",
+    incorrect_answers: [
+        "A) A vehicle",
+        "B) An animal",
+        "C) A tree"
+    ]
+},
+{
+    category: "Computer Science",
+    type: "multiple",
+    difficulty: "easy",
+    question: "Which of these is a computer brand?",
+    correct_answer: "A) Dell",
+    incorrect_answers: [
+        "B) Ferrari",
+        "C) Samsung",
+        "D) Nike"
+    ]
+},
+{
+    category: "Computer Science",
+    type: "multiple",
+    difficulty: "easy",
+    question: "Which of these is not an operating system?",
+    correct_answer: "D) Linux",
+    incorrect_answers: [
+        "A) Android",
+        "B) iOS",
+        "C) Windows"
+    ]
+},
+{
+    category: "Computer Science",
+    type: "multiple",
+    difficulty: "easy",
+    question: "Which of these is not a file type?",
+    correct_answer: "A) .computer",
+    incorrect_answers: [
+        "B) .doc",
+        "C) .jpg",
+        "D) .mp3"
+    ]
+},
+{
+    category: "Computer Science",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What does the term 'Network' represent in computing?",
+    correct_answer: "D) A collection of interconnected nodes",
+    incorrect_answers: [
+        "A) A sweater",
+        "B) A column",
+        "C) A fishing spot"
+    ]
+},
+{
+    category: "Computer Science",
+    type: "multiple",
+    difficulty: "easy",
+    question: "Which of these is a programming language?",
+    correct_answer: "D) Python",
+    incorrect_answers: [
+        "A) English",
+        "B) Italian",
+        "C) Spanish"
+    ]
+},
+{
+    category: "Computer Science",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Programming is a fundamental component of computer science",
+    correct_answer: "true"
+},
+{
+    category: "Computer Science",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The mouse is a common input device associated with a computer",
+    correct_answer: "true"
+},
+{
+    category: "Computer Science",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Computers solve mathematical problems much more efficiently than humans",
+    correct_answer: "true"
+},
+{
+    category: "Computer Science",
+    type: "boolean",
+    difficulty: "easy",
+    question: "HTML is a programming language used to create web pages",
+    correct_answer: "false"
+},
+{
+    category: "Computer Science",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Algorithms are step-by-step procedures for solving a specific problem",
+    correct_answer: "true"
+},
+
+]
